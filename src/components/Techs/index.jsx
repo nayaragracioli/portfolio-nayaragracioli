6d57db3c62fdbb0ffe7a8cd016be93ffb509b475
nayaragracioli <nayaragraciolisa@gmail.com>
2023-10-27 10:07:56 -0300

    feat: adding responsiveness

--- src/components/Techs/index.jsx
+++ src/components/Techs/index.jsx
@@ -1,10 +1,10 @@
 import Styles from "./style.module.scss";
-import {FaHtml5, FaCss3Alt, FaReact, FaGitAlt, FaGithub} from "react-icons/fa";
-import {SiJavascript, SiSass, SiTailwindcss} from "react-icons/si";
+import { FaHtml5, FaCss3Alt, FaReact, FaGitAlt, FaGithub } from "react-icons/fa";
+import { SiJavascript, SiSass, SiTailwindcss } from "react-icons/si";
 
 export const Techs = () => {
 
-    return(
+    return (
         <section className={`container ${Styles.techBox}`}>
             <div>
                 <div className={Styles.infoTech}>
@@ -18,14 +18,38 @@ export const Techs = () => {
                 </div>
                 <div className={Styles.imgTech}>
                     <ul>
-                        <li><FaHtml5 size={27}/></li>
-                        <li><FaCss3Alt size={27}/></li>
-                        <li><SiJavascript size={27}/></li>
-                        <li><FaReact size={27}/></li>
-                        <li><SiSass size={27}/></li>
-                        <li><SiTailwindcss size={27}/></li>
-                        <li><FaGitAlt size={27}/></li>
-                        <li><FaGithub size={27}/></li>
+                        <li>
+                            <FaHtml5 size={40} color="#EC6231" />
+                            <p className="paragraph">HTML5</p>
+                        </li>
+                        <li>
+                            <FaCss3Alt size={40} color="rgb(41 169 223)" />
+                            <p className="paragraph">CSS3</p>
+                        </li>
+                        <li>
+                            <SiJavascript size={40} color="rgb(249 224 62)" />
+                            <p className="paragraph">JavaScript</p>
+                        </li>
+                        <li>
+                            <FaReact size={40} color="rgb(97 219 251)" />
+                            <p className="paragraph">React</p>
+                        </li>
+                        <li>
+                            <SiSass size={40} color="rgb(204 102 153)" />
+                            <p className="paragraph">SASS</p>
+                        </li>
+                        <li>
+                            <SiTailwindcss size={40} color="rgb(56 189 248)" />
+                            <p className="paragraph">Tailwind</p>
+                        </li>
+                        <li>
+                            <FaGitAlt size={40} color="rgb(240 80 51)" />
+                            <p className="paragraph">Git</p>
+                        </li>
+                        <li>
+                            <FaGithub size={40} color="rgb(61 117 194)" />
+                            <p className="paragraph">GitHub</p>
+                        </li>
                     </ul>
                 </div>
             </div>
