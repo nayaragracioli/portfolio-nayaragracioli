8c689145940ddbeee5a24b9e707355941ab9b970
nayaragracioli <nayaragraciolisa@gmail.com>
2024-03-05 14:32:33 -0300

    feat: Change the order about me

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -11,52 +11,16 @@ export const AboutMe = () => {
                 </div>
                 <div className={Styles.experience__sections}>
                     <div className={Styles.experience__content}>
-                        <div className={Styles.experience__data}>
+                    <div className={Styles.experience__data}>
                             <div>
-                                <h3>2015</h3>
-                            </div>
-                            <div>
-                                <span className={Styles.experience__rounder}></span>
-                                <span className={Styles.experience__line}></span>
-                            </div>
-                            <div className={Styles.experience__text}> 
-                                <p>Beginning of the Graduation in Materials Engineering at UFSC.</p>
-                            </div>
-                        </div>
-                        <div className={Styles.experience__data}>
-                            <div>
-                                <h3>2019</h3>
+                                <h3>2023</h3>
                             </div>
                             <div>
                                 <span className={Styles.experience__rounder}></span>
                                 <span className={Styles.experience__line}></span>
                             </div>
                             <div className={Styles.experience__text}>
-                                <p>1 year of volunteering at Empresa Jr, initially working as an administrative advisor and then as vice president.</p>
-                            </div>
-                        </div>
-                        <div className={Styles.experience__data}>
-                            <div>
-                                <h3>2021</h3>
-                            </div>
-                            <div>
-                                <span className={Styles.experience__rounder}></span>
-                                <span className={Styles.experience__line}></span>
-                            </div>
-                            <div className={Styles.experience__text}>
-                                <p>1 year internship in Manufacturing Engineering, working with continuous improvement projects.</p>
-                            </div>
-                        </div>
-                        <div className={Styles.experience__data}>
-                            <div>
-                                <h3>2022</h3>
-                            </div>
-                            <div>
-                                <span className={Styles.experience__rounder}></span>
-                                <span className={Styles.experience__line}></span>
-                            </div>
-                            <div className={Styles.experience__text}>
-                                <p>After graduating, I started as an engineering analyst.</p>
+                                <p>I was invited by Kenzie to be a frontend monitor for a period of 2 months.</p>
                             </div>
                         </div>
                         <div className={Styles.experience__data}>
@@ -73,14 +37,50 @@ export const AboutMe = () => {
                         </div>
                         <div className={Styles.experience__data}>
                             <div>
-                                <h3>2023</h3>
+                                <h3>2022</h3>
                             </div>
                             <div>
                                 <span className={Styles.experience__rounder}></span>
                                 <span className={Styles.experience__line}></span>
                             </div>
                             <div className={Styles.experience__text}>
-                                <p>I was invited by Kenzie to be a frontend monitor for a period of 2 months.</p>
+                                <p>After graduating, I started as an engineering analyst.</p>
+                            </div>
+                        </div>
+                        <div className={Styles.experience__data}>
+                            <div>
+                                <h3>2021</h3>
+                            </div>
+                            <div>
+                                <span className={Styles.experience__rounder}></span>
+                                <span className={Styles.experience__line}></span>
+                            </div>
+                            <div className={Styles.experience__text}>
+                                <p>1 year internship in Manufacturing Engineering, working with continuous improvement projects.</p>
+                            </div>
+                        </div>
+                        <div className={Styles.experience__data}>
+                            <div>
+                                <h3>2019</h3>
+                            </div>
+                            <div>
+                                <span className={Styles.experience__rounder}></span>
+                                <span className={Styles.experience__line}></span>
+                            </div>
+                            <div className={Styles.experience__text}>
+                                <p>1 year of volunteering at Empresa Jr, initially working as an administrative advisor and then as vice president.</p>
+                            </div>
+                        </div>
+                        <div className={Styles.experience__data}>
+                            <div>
+                                <h3>2015</h3>
+                            </div>
+                            <div>
+                                <span className={Styles.experience__rounder}></span>
+                                <span className={Styles.experience__line}></span>
+                            </div>
+                            <div className={Styles.experience__text}> 
+                                <p>Beginning of the Graduation in Materials Engineering at UFSC.</p>
                             </div>
                         </div>
                     </div>
